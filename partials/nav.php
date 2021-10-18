8b86357c4cb15815ca4bb2376f9f9403bcde2504
cjdomacena <cjdomacena@yahoo.com>
2021-10-18 00:25:13 -0400

    Moved script and stylesheet position inside code

--- partials/nav.php
+++ partials/nav.php
@@ -1,5 +1,4 @@
-<script src="./../public_html/Project/helper.js"></script>
-<link rel="stylesheet" href="./../public_html/Project/style.css">
+
 <?php
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
@@ -25,10 +24,13 @@ session_start();
 require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
+<script src="./../public_html/Project/helper.js"></script>
+<link rel="stylesheet" href="./../public_html/Project/style.css">
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Home</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
