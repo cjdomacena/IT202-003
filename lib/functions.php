9b5935af939e233bdae43c311d82b8f0ff814210
cjdomacena <cjdomacena@yahoo.com>
2021-10-18 00:17:49 -0400

    Updated is_logged_in()

--- lib/functions.php
+++ lib/functions.php
@@ -36,9 +36,14 @@ function is_valid_email($email = "")
     return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
 }
 //TODO 3: User Helpers
-function is_logged_in()
+function is_logged_in($redirect = false, $destination = "login.php")
 {
-    return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
+    $isLoggedIn = isset($_SESSION["user"]);
+    if ($redirect && !$isLoggedIn) {
+        flash("You must be logged in to view this page", "warning");
+        die(header("Location: $destination"));
+    }
+    return $isLoggedIn; //se($_SESSION, "user", false, false);
 }
 function has_role($role)
 {
@@ -98,7 +103,6 @@ function reset_session()
 {
     session_unset();
     session_destroy();
-    session_start();
 }
 function users_check_duplicate($errorInfo)
 {
