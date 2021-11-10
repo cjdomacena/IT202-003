23574131ad4280d62c068224085cd8f2e0cf35d4
cjdomacena <cjdomacena@yahoo.com>
2021-11-10 04:19:12 -0500

    Added styles for create roles

--- public_html/Project/admin/create_role.php
+++ public_html/Project/admin/create_role.php
@@ -28,18 +28,34 @@ if (isset($_POST["name"]) && isset($_POST["description"])) {
     }
 }
 ?>
-<h1>Create Role</h1>
-<form method="POST">
+<div class=" mx-auto mt-5 p-4 space-y-4">
     <div>
-        <label for="name">Name</label>
-        <input id="name" name="name" required />
+        <h1 class="text-xl">Create Role</h1>
     </div>
-    <div>
-        <label for="d">Description</label>
-        <textarea name="description" id="d"></textarea>
-    </div>
-    <input type="submit" value="Create Role" />
-</form>
+    <form method="POST" class="space-y-4">
+        <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+                <label for="role">Role</label>
+                <input type="role" id="role" name="role" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-2" placeholder="Admin" />
+            </div>
+        </div>
+        <div>
+            <label for="role_description">Description</label>
+            <textarea name="role_description" id="role_description" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-2"></textarea>
+        </div>
+        <div>
+            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" value="Login">
+                <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <!-- Heroicon name: solid/lock-closed -->
+                    <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+                Create Role
+            </button>
+        </div>
+    </form>
+</div>
 <?php
 //note we need to go up 1 more directory
 require_once(__DIR__ . "/../../../partials/flash.php");
