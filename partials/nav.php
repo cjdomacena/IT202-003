e78b3e415886d7430bdea055caf2e7f2afc0294b
cjdomacena <cjdomacena@yahoo.com>
2021-11-10 06:20:58 -0500

    Moved script to utils

--- partials/nav.php
+++ partials/nav.php
@@ -28,9 +28,8 @@ require_once(__DIR__ . "/../lib/functions.php");
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <!-- include css and js files -->
 <link rel="stylesheet" href="<?php echo get_url('tailwind.css') ?>">
+<script src="<?php echo get_url("utils/index.js") ?>" defer></script>
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
-
-
 <header class="w-full">
     <nav class="container-2xl bg-indigo-600 p-4 flex justify-between px-8 shadow">
         <div id="logo" class="h-8">
@@ -65,7 +64,6 @@ require_once(__DIR__ . "/../lib/functions.php");
             <?php if (is_logged_in()) : ?>
                 <li class="hover:text-indigo-200 text-red-400"><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
             <?php endif; ?>
-
             <li class="border-l-2 pl-3 border-gray-900  border-opacity-25 hover:text-gray-400"><a href="/cart" class="flex">
                     <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path>
@@ -75,78 +73,3 @@ require_once(__DIR__ . "/../lib/functions.php");
         </ul>
     </nav>
 </header>
-<script>
-    // Grab the <a> with id of profile-link
-    const profileLink = document.getElementById("profile-link");
-
-    // Grab collapsible-nav 
-    const collapse = document.getElementById("collapsible-nav");
-
-    // Create a ul element
-    // This will live inside the collapsible-nav
-    const ul = document.createElement("ul");
-
-    // Initialize the links that will be displayed in the collapsible-nav
-    const menuItems = ["Edit Email", "Edit Username", "Edit Password"];
-
-    // Loop through the menu items to create new <li> element
-    for (let i = 0; i < menuItems.length; i++) {
-
-        // Create new <li>
-        const li = document.createElement("li");
-
-        // Create new <a>
-        // This will live inside the <li> element
-        const link = document.createElement("a");
-
-        // Create a new text node for the link
-        link.appendChild(document.createTextNode(menuItems[i]));
-
-        // Clean the initial menuItems (menuItems[i]) to be used as a href attribute
-        const cleanedLink = menuItems[i].toLowerCase();
-
-        // Will be split with an underscore(_)
-        link.href = cleanedLink.split("_");
-
-        // Add the link inside the <li> element as its child
-        li.appendChild(link);
-
-        // Add classes
-        li.classList.add("cursor-pointer");
-
-        li.classList.add("hover:text-indigo-900");
-
-        // Add the <li> that contains <a> as its child to the previously created <ul>
-        ul.appendChild(li);
-    }
-
-    // Track user click events WITHIN the document
-    // This will allow simple functionality for the dropdown to be closed when user clicked outside of the "Profile" link to toggle the collapsed-nav.
-    document.addEventListener("click", (e) => {
-
-        // If the profileLink is the same as the (e) that was given by the event listener
-        if (!profileLink.contains(e.target)) {
-
-            // Remove the visible class
-            collapse.classList.remove("visible");
-
-            // Make the collapsible-nav invisible
-            collapse.classList.add("invisible");
-
-        } else {
-            
-            // Toggle between invisible and visible
-            collapse.classList.toggle("invisible");
-
-
-            collapse.classList.toggle("visible");
-
-
-            collapse.appendChild(ul);
-        }
-    })
-
-    // Just add styles to the <ul>
-    ul.classList.add("space-y-4");
-    ul.classList.add("text-indigo-600");
-</script>
