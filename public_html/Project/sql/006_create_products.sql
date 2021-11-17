ce462765c725215b25d829002ba75c4c3f0aa428
cjdomacena <cjdomacena@yahoo.com>
2021-11-17 06:53:56 -0500

    Fixed Table

--- public_html/Project/sql/006_create_products.sql
+++ public_html/Project/sql/006_create_products.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS Products(
-  id int AUTO_INCREMENT NOT NULL,
+  id int AUTO_INCREMENT not NULL,
   name VARCHAR(30) UNIQUE,
   description text,
   stock int DEFAULT 0,
   cost int DEFAULT 99999,
   image text,
   created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP PRIMARY KEY(id)
+  modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY(id),
+  FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
+  FOREIGN KEY (`product_id`) REFERENCES Roles(`id`),
+  UNIQUE KEY (`user_id`, `product_id`)
 )
--- public_html/Project/sql/007_user_products.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS UserProducts (
-  id int AUTO_INCREMENT not NULL FOREIGN KEY(user_id) REFERENCES Users(id) FOREIGN KEY (product_id) REFERENCES Products(id)
-)
