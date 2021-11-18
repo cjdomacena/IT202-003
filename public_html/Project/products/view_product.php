83ef469e8c62e3f6c44a4abc51c342cda22d8c13
cjdomacena <cjdomacena@yahoo.com>
2021-11-18 08:04:31 -0500

    added view products page

---
+++ public_html/Project/products/view_product.php
@@ -0,0 +1,21 @@
+<?php
+require(__DIR__ . "/../../../partials/nav.php");
+
+$product = [];
+if(isset($_GET["id"]))
+{
+	$id = se($_GET,"id", -1, false);
+	if($id !=1)
+	{
+		$db = getDB();
+		$stmt = $db->prepare('SELECT * FROM Products WHERE id = :id');
+		$stmt->execute([':id' => $id]);
+		$product = $stmt->fetch(PDO::FETCH_ASSOC);
+	}
+
+}
+
+echo '<pre>' . var_export($product, true) . '</pre>';
+?>
+
+
