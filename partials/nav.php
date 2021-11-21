f53b2ace7e82be09695799309ff08a79c33a8a26
cjdomacena <cjdomacena@yahoo.com>
2021-11-21 08:47:00 -0500

    Edited nav to show current cart count

--- partials/nav.php
+++ partials/nav.php
@@ -91,9 +91,8 @@ require_once(__DIR__ . "/../lib/functions.php");
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
-                    Cart
                     <span class="text-sm font-medium inline-flex items-center p-1 rounded-full mr-2 w-4 h-4 text-center align-middle">
-                        <p class="text-center mx-auto">6</p>
+                        <p class="text-center mx-auto" id="cart-count"></p>
                     </span>
                 </a>
             </li>
