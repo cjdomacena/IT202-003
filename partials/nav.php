00511d0610b51714b337f477b3f9ec30dacf7bc7
cjdomacena <cjdomacena@yahoo.com>
2021-11-21 07:46:04 -0500

    Added dropdown for roles and count for cart

--- partials/nav.php
+++ partials/nav.php
@@ -59,7 +59,7 @@ require_once(__DIR__ . "/../lib/functions.php");
                                 <a href="<?php echo get_url('profile.php'); ?>" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Edit Profile</a>
                             </li>
                             <li>
-                                <a href="<?php echo get_url('account/reset_password.php'); ?>" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Reset Password</a>
+                                <a href="<?php echo get_url('account/reset_password.php'); ?>" class="text-sm hover:bg-gray-100 text-red-400 block px-4 py-2">Reset Password</a>
                             </li>
                         </ul>
                     </div>
@@ -70,20 +70,33 @@ require_once(__DIR__ . "/../lib/functions.php");
                 <li class="hover:text-indigo-200"><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
             <?php endif; ?>
             <?php if (has_role("admin")) : ?>
-                <!-- <li class="hover:text-indigo-200"><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
-                <li class="hover:text-indigo-200"><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
-                <li class="hover:text-indigo-200"><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li> -->
-
+                <li>
+                    <button id="dropdownButton-role" data-dropdown-toggle="dropdown-role" class="text-white hover:text-indigo-200 font-medium rounded-lg pl-0 pr-2 py-2.5 text-center inline-flex items-center" type="button">Roles<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg></button>
+                    <div id="dropdown-role" class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44">
+                        <ul class="py-1" aria-labelledby="dropdownButton-role">
+                            <li class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
+                            <li class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
+                            <li class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+                        </ul>
+                    </div>
+                </li>
             <?php endif; ?>
             <?php if (is_logged_in()) : ?>
-                <li class="hover:text-indigo-200 text-red-400"><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+                <li class="hover:text-indigo-200 text-red-500"><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
             <?php endif; ?>
-            <li class="border-l-2 pl-3 border-gray-900  border-opacity-25 hover:text-gray-400"><a href="/cart" class="flex">
-                    <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path>
+            <li class="border-l-2 pl-3 border-gray-900  border-opacity-25 hover:text-gray-400 text-white">
+                <a href="#" class="flex">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                     </svg>
                     Cart
-                </a> </li>
+                    <span class="text-sm font-medium inline-flex items-center p-1 rounded-full mr-2 w-4 h-4 text-center align-middle">
+                        <p class="text-center mx-auto">6</p>
+                    </span>
+                </a>
+            </li>
         </ul>
     </nav>
 </header>
