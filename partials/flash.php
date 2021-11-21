8f5e04ee3d58b589a434c8a81b66d1901859f2a6
cjdomacena <cjdomacena@yahoo.com>
2021-11-21 10:39:15 -0500

    removed space

--- partials/flash.php
+++ partials/flash.php
@@ -6,7 +6,7 @@
     <?php $messages = getMessages(); ?>
     <?php if ($messages) : ?>
         <?php foreach ($messages as $msg) : ?>
-            <div class="min-w-min p-4 <?php se($msg, 'color', 'info'); ?> rounded my-4 ">
+            <div class="min-w-min p-4 <?php se($msg, 'color', 'info'); ?> rounded my-4">
                 <div class="alert alert-<?php se($msg, 'color', 'info'); ?>" role="alert"><?php se($msg, "text", ""); ?></div>
             </div>
         <?php endforeach; ?>
@@ -30,4 +30,11 @@
     }
 
     moveMeUp(document.getElementById("flash"));
+
+
+    function check() {
+            if ("<?php echo is_logged_in() ?>") {
+                flash("Welcome! <?php echo get_username()?>", "bg-green-200", 1000, "fade");
+            }
+    }
 </script>
