ce4768a2b9154f385870146e763c270b709b0e2c
cjdomacena <cjdomacena@yahoo.com>
2021-11-26 20:19:34 -0500

    Feat: Styling

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -1,21 +1,25 @@
 <?php
-require_once(__DIR__ . '/../../partials/nav.php')
+require_once(__DIR__ . '/../../partials/nav.php');
+if (!is_logged_in()) {
+	die(header("Location: " . get_url('index.php')));
+}
 ?>
 
 <div class="container mx-auto my-16">
-	<div id="data">
+	<div id="data-cart">
 
 	</div>
 </div>
 
 
 <script>
+	get_cart_count()
 	$(document).ready(
 		$.ajax({
 			type: "GET",
 			url: "./cart/view_cart.php",
 			success: (data) => {
-				$("#data").html(data)
+				$("#data-cart").html(data)
 			}
 		})
 	)
--- public_html/Project/cart/view_cart.php
+++ public_html/Project/cart/view_cart.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . "../../../../lib/functions.php");
 require_once(__DIR__ . "../../../../lib/db.php");
 session_start();
+$total = 0;
 if (!is_logged_in()) {
 	die(header("Location: " . get_url('index.php')));
 }
@@ -26,15 +27,14 @@ if (isset($_POST["quantity"]) && isset($_POST["cart"]) && !isset($_POST["type"])
 	} catch (PDOException $e) {
 		flash("Something went wrong...", "bg-red-200");
 	}
-} 
-else if(isset($_POST["cart"]) && isset($_POST["type"])){
+} else if (isset($_POST["cart"]) && isset($_POST["type"])) {
 	$db = getDB();
 	$cartID = $_POST["cart"];
 	$stmt = $db->prepare('DELETE FROM Cart WHERE id = :cart_id');
-	try{
+	try {
 		$stmt->execute([":cart_id" => $cartID]);
 		flash("Successfully Removed from cart", "bg-red-200");
-	}catch(PDOException $e){
+	} catch (PDOException $e) {
 		flash("Something Went wrong...", "bg-red-200");
 	}
 	$stmt = $db->prepare('SELECT Cart.id as cart_id, Products.name, Products.description, Products.image, Products.cost,Products.id, Cart.quantity FROM((Products INNER JOIN Cart ON Products.id = Cart.product_id) INNER JOIN Users ON Users.id = :id)');
@@ -45,8 +45,7 @@ else if(isset($_POST["cart"]) && isset($_POST["type"])){
 	} catch (PDOException $e) {
 		flash("Something went wrong...", "bg-red-200");
 	}
-}
-else {
+} else {
 	$id = get_user_id();
 	$db = getDB();
 	$stmt = $db->prepare('SELECT Cart.id as cart_id, Products.name, Products.description, Products.image, Products.cost,Products.id, Cart.quantity FROM((Products INNER JOIN Cart ON Products.id = Cart.product_id) INNER JOIN Users ON Users.id = :id)');
@@ -60,29 +59,39 @@ else {
 }
 
 ?>
-
-<div class="w-2/3 mx-auto">
-	<div class="my-8 py-4 bg-gray-100 rounded border">
-		<h1 class="ml-4">View Cart</h1>
-	</div>
-	<div class=" grid-cols-1 space-y-4 ">
-		<?php foreach ($r as $index => $item) : ?>
-			<div class="bg-gray-100 p-4">
-				<div class="flex justify-between">
-					<div>
-						<h1 class="text-xl font-bold"><?php echo $item['name'] ?></h1>
-						<p class="truncate"><?php echo $item['description'] ?></p>
-
+<div class="flex space-x-4 justify-between">
+	<div class="w-8/12 mx-auto">
+		<div class="my-8 py-4 bg-gray-100 rounded border">
+			<h1 class="ml-4">View Cart</h1>
+		</div>
+		<div class=" grid-cols-1 divide-solid divide-y-2 my-4">
+			<?php foreach ($r as $index => $item) : ?>
+				<?php $total += $item["cost"] * $item["quantity"] ?>
+				<div class="hover:bg-gray-100 p-4">
+					<div class="flex justify-between">
+						<div class="flex mb-4">
+							<img class="w-24 h-24 object-fill rounded mr-4" src="<?php echo $item["image"] ?>" />
+							<div>
+								<h1 class="text-xl font-bold"><?php echo $item['name'] ?></h1>
+								<p class="truncate"><?php echo $item['description'] ?></p>
+							</div>
+						</div>
+						<div>
+							<p>Total: $<?php echo $item["cost"] * $item["quantity"] ?></p>
+							<input type="number" min=0 max=99 value="<?php echo $item["quantity"]; ?>" name="quantity" id="quantity" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 my-4 w-24" />
+						</div>
 					</div>
-					<div>
-						<p>Total: $<?php echo $item["cost"] * $item["quantity"] ?></p>
-						<input type="number" min=0 max=99 value="<?php echo $item["quantity"]; ?>" name="quantity" id="quantity" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 my-4 w-24" />
+					<div class="flex space-x-4 mt-4">
+						<button class="text-indigo-800" onclick="update_qty(<?php echo $item['cart_id'] ?>)">Update</button>
+						<button class="text-red-600" onclick="remove_item(<?php echo $item['cart_id'] ?>)">Remove</button>
 					</div>
 				</div>
-				<button onclick="update_qty(<?php echo $item['cart_id'] ?>)">Update</button>
-				<button onclick="remove_item(<?php echo $item['cart_id'] ?>)">Remove</button>
-			</div>
+			<?php endforeach; ?>
 
-		<?php endforeach; ?>
+
+		</div>
+	</div>
+	<div class="w-3/12 my-8 py-4 rounded">
+		<h3 class="p-4">Total: $<?php echo $total ?></h3>
 	</div>
 </div>
